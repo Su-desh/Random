a7ca5a762ebfa2b92ae432464c0591a761e41339
sudesh <sudesh1428@gmail.com>
2023-09-19 10:29:18 +0530

    web icon change

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,6 @@ import 'package:connectivity_plus/connectivity_plus.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'package:flutter_windowmanager/flutter_windowmanager.dart';
 import 'package:get/get.dart';
 import 'package:random/API/api.dart';
 import 'package:random/chat/conversations/state_conversation.dart';
@@ -18,8 +17,8 @@ import 'firebase_options.dart';
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   //prevent user from taking screenshot
-  // ! i will uncomment this at production, it is giving error for web build
-  await FlutterWindowManager.addFlags(FlutterWindowManager.FLAG_SECURE);
+  // ! it is not supporting web so commenting it, in future i will use this.
+  // await FlutterWindowManager.addFlags(FlutterWindowManager.FLAG_SECURE);
 
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   sharedPreferences = await SharedPreferences.getInstance();
